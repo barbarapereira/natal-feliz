d05d22b3ed83f2a04214ae0f613d3aea8bb6dfd6
Barbara Pereira <barbara.pereira1990@gmail.com>
2022-12-07 10:59:40 -0300

    ajuste diretorio e validacao vincular padrinho

--- models/Crianca.js
+++ models/Crianca.js
@@ -1,32 +1,44 @@
 const Pessoa = require("./Pessoa.js");
 const Padrinho = require("./Padrinho");
 
-class Crianca extends Pessoa{
-    nomeDoResponsavel = "";
-    idade = "";
-    genero = "";
-    tamanhoRoupa = "";
-    tamanhoSapato = "";
-    padrinhoCadastrado = [];
+class Crianca extends Pessoa {
+  nomeDoResponsavel = "";
+  idade = "";
+  genero = "";
+  tamanhoRoupa = "";
+  tamanhoSapato = "";
+  padrinhoCadastrado = [];
 
-    constructor(nomeCompleto, contato, nomeDoResponsavel, idade, genero, tamanhoRoupa, tamanhoSapato){
-        super(nomeCompleto, contato);
-        this.nomeDoResponsavel = nomeDoResponsavel;
-        this.idade = idade;
-        this.genero = genero;
-        this.tamanhoRoupa = tamanhoRoupa;
-        this.tamanhoSapato = tamanhoSapato;
+  constructor(
+    nomeCompleto,
+    contato,
+    nomeDoResponsavel,
+    idade,
+    genero,
+    tamanhoRoupa,
+    tamanhoSapato
+  ) {
+    super(nomeCompleto, contato);
+    this.nomeDoResponsavel = nomeDoResponsavel;
+    this.idade = idade;
+    this.genero = genero;
+    this.tamanhoRoupa = tamanhoRoupa;
+    this.tamanhoSapato = tamanhoSapato;
 
-        Pessoa.listaParticipante.criancas.push(this);
+    Pessoa.listaParticipante.criancas.push(this);
+  }
+
+  vincularPadrinho(padrinho) {
+    if (padrinho instanceof Padrinho) {
+      //  cada padrinho será único, só apadrinhará 1 criaça
+      if (this.padrinhoCadastrado.length != 0) {
+        console.log("a criança já possui um padrinho");
+      } else {
+        return this.padrinhoCadastrado.push(padrinho.id);
+      }
+      //verificar na lista de crianças quais padrinhos estão vinculados(retorna a lista de padrinhos vinculados)
     }
-
-    vincularPadrinho(padrinho){
-        if(padrinho instanceof Padrinho){
-            this.padrinhoCadastrado.push(padrinho);
-        }
-    }
+  }
 }
-
-
 
 module.exports = Crianca;
